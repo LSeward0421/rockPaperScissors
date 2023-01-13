926d235cca8ffbdfcb40f1c99f1a4db77c403907
Lauren Seward <lauren.unal@gmail.com>
2023-01-13 09:17:59 -0600

    Feat: Add basic logic to player class

---
+++ main.js
@@ -0,0 +1,5 @@
+var human = new Player({ name: 'Human', emoji: '😎' })
+var computer = new Player({ name: 'Computer', emoji: '👾' })
+
+
+// need randomize function
--- player.js
+++ player.js
@@ -6,10 +6,17 @@ class Player {
     this.numWins = 0;
   }
   takeTurn() {
+    if (this.name === game.turn) {
+      game.turn = game.players[1].name;
+    } else {
+      game.turn = this.name;
+    };
+  };
+ };
+
     // Always human vs comp which is two instances of player
     // can access game properties 
     // if the player name is strictly equal to game this.turn
       // THEN this.turn is assigned to that first players name
     // If NOT then it is the opponent's turn
-  }
-}
+ 
